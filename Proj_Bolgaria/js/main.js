9dcb159e0eaa653e19bd3874d1d2447268e78697
SirotyukD <sirotuk93@gmail.com>
2018-11-28 18:40:19 +0200

    Bolgaria add php form

--- Proj_Bolgaria/js/main.js
+++ Proj_Bolgaria/js/main.js
@@ -1,3 +1,20 @@
 $('.reviews-wrap').slick({
 	dots: true
 });
+
+//E-mail Ajax Send
+$("#form").submit(function() { //Change
+	var th = $(this);
+	$.ajax({
+		type: "POST",
+		url: "php/mail.php", //Change
+		data: th.serialize()
+	}).done(function() {
+		alert("Спасибо за обращение! В ближайшее время ми с вами свяжемся");
+		setTimeout(function() {
+			// Done Functions
+			th.trigger("reset");
+		}, 1000);
+	});
+	return false;
+});
