5d46ccf37361846fd459860fc4c59eac0663e5d0
Dima <sirotuk93@gmail.com>
2018-12-12 22:32:56 +0200

    add Gulp

---
+++ Gulp/Gulp4_Lavrik/src/js/some.js
@@ -0,0 +1,4 @@
+console.log(divide(4, 2));
+console.log(divide(6, 2));
+
+
---
+++ Gulp/Gulp4_Wayup/gulpfile.js
@@ -0,0 +1,119 @@
+'use strict';
+
+    const gulp = require('gulp');
+    const mainBowerFiles = require('gulp-main-bower-files');
+    const watch = require('gulp-watch');
+    const prefixer = require('gulp-autoprefixer');
+    const uglify = require('gulp-uglify');
+    const sourcemaps = require('gulp-sourcemaps');
+    const sass = require('gulp-sass');
+    const cleanCSS = require('gulp-clean-css');
+    const imagemin = require('gulp-imagemin');
+    const pngquant = require('imagemin-pngquant');
+    const rimraf = require('rimraf');
+    const browserSync = require("browser-sync");
+	const reload = browserSync.reload;
+	
+	var path = {
+		vendor: {
+			js: 'app/js/',
+			css: 'app/css/'
+		},
+		dist: { //Тут мы укажем куда складывать готовые после сборки файлы
+			html: 'dist/',
+			js: 'dist/js/',
+			scss: 'dist/css/',
+			css: 'dist/css/',
+			img: 'dist/img/',
+			fonts: 'dist/fonts/'
+		},
+		app: { //Пути откуда брать исходники
+			html: 'app/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+			js: 'app/js/*.js',//В стилях и скриптах нам понадобятся только main файлы
+			scss: 'app/css/*.scss',
+			css: 'app/css/*.css',
+			img: 'app/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+			fonts: 'app/fonts/**/*.*'
+		},
+		watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+			html: 'app/**/*.html',
+			js: 'app/js/**/*.js',
+			scss: 'app/css/**/*.scss',
+			css: 'app/css/**/*.css',
+			img: 'app/img/**/*.*',
+			fonts: 'app/fonts/**/*.*'
+		},
+		clean: './dist'
+	};
+
+	var config = {
+		server: {
+			baseDir: "./dist"
+		},
+		tunnel: true,
+		host: 'localhost',
+		port: 8081,
+		logPrefix: "WAYUP"
+	};
+
+	gulp.task('vendorJs:build', function () {
+		return gulp.src( mainBowerFiles('**/*.js') ) //Выберем файлы по нужному пути
+			.pipe(gulp.dest(path.vendor.js)) //Выплюнем готовый файл в app
+	});
+	
+	gulp.task('vendorCss:build', function () {
+   		 return gulp.src( mainBowerFiles('**/*.css') ) //Выберем файлы по нужному пути
+        .pipe(gulp.dest(path.vendor.css)) //И в app
+});
+
+gulp.task('html:build', function () {
+    return gulp.src(path.app.html) //Выберем файлы по нужному пути
+        .pipe(gulp.dest(path.dist.html)) //Выплюнем их в папку build
+        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
+});
+
+gulp.task('js:build', function () {
+    return gulp.src(path.app.js) //Найдем наш main файл
+        .pipe(sourcemaps.init()) //Инициализируем sourcemap
+        .pipe(uglify()) //Сожмем наш js
+        .pipe(sourcemaps.write()) //Пропишем карты
+        .pipe(gulp.dest(path.dist.js)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+});
+
+gulp.task('scss:build', function () {
+    return gulp.src(path.app.scss) //Выберем наш main.scss
+        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(sass()) //Скомпилируем
+        .pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(cleanCSS()) //Сожмем
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.dist.scss)) //И в build
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('css:build', function () {
+    return gulp.src(path.app.css) //Выберем наш main.css
+        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(gulp.dest(path.dist.css)) //И в build
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', function () {
+    return gulp.src(path.app.img) //Выберем наши картинки
+        .pipe(imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.dist.img)) //И бросим в build
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', function() {
+    return gulp.src(path.app.fonts)
+        .pipe(gulp.dest(path.dist.fonts))
+});
+
+gulp.task('build', gulp.series('vendorCss:build', 'vendorJs:build', 'html:build', 'js:build', 'scss:build', 'css:build', 'fonts:build', 'image:build'));
