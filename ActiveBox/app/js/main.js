58f6bb22aa809f03dfe207361bf1f20b1516fb96
SirotyukD <sirotuk93@gmail.com>
2019-02-14 13:19:34 +0200

    activebox

--- ActiveBox/app/js/main.js
+++ ActiveBox/app/js/main.js
@@ -4,30 +4,8 @@ $(function() {
 
 	//slider
 
-	$('.slider-slick').slick({
-		centerMode: true,
-		centerPadding: '20px',
-		slidesToShow: 1,
-		responsive: [
-		  {
-			breakpoint: 768,
-			settings: {
-			  arrows: false,
-			  centerMode: true,
-			  centerPadding: '10px',
-			  slidesToShow: 1
-			}
-		  },
-		  {
-			breakpoint: 480,
-			settings: {
-			  arrows: false,
-			  centerMode: true,
-			  centerPadding: '10px',
-			  slidesToShow: 1
-			}
-		  }
-		]
+	$('.slick-slider').slick({
+		dots: true		
 		});
 
 		//tabs
