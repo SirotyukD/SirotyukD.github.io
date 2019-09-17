c7e1b4c32eb08cfe39b7298da7ced6e29a8913df
SirotyukD <sirotuk93@gmail.com>
2019-09-17 18:50:39 +0300

    Add Pets

---
+++ Pets/app/js/main.js
@@ -0,0 +1,33 @@
+$(function () {
+
+	// Custom JS
+
+	//плавные якоря
+	$("#menu, #nav").on("click", "a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		var id = $(this).attr('href'),
+			//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({
+			scrollTop: top
+		}, 1000);
+	});
+
+	//m-menu
+	$('.m-menu').on('click', function (e) {
+		e.preventDefault();
+		$('.m-menu-box').toggleClass('show');
+	});
+
+	//slick слайдер
+	$('.pets-wrap-slider').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1
+	});
+
+
+});
