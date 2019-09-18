b98a60bf42561ffa0e0ea6af767caf6129168f92
SirotukD <sirotuk93#gmail.com>
2019-09-18 21:15:18 +0300

    add Pets v2

--- Pets/app/js/main.js
+++ Pets/app/js/main.js
@@ -2,19 +2,31 @@ $(function () {
 
 	// Custom JS
 
-	//плавные якоря
-	$("#menu, #nav").on("click", "a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
-		//забираем идентификатор бока с атрибута href
-		var id = $(this).attr('href'),
-			//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({
-			scrollTop: top
+	//плавные якоря для перехода внутри сайта
+	// $("#menu, #nav").on("click", "a", function (event) {
+	// 	//отменяем стандартную обработку нажатия по ссылке
+	// 	event.preventDefault();
+	// 	//забираем идентификатор бока с атрибута href
+	// 	var id = $(this).attr('href'),
+	// 		//узнаем высоту от начала страницы до блока на который ссылается якорь
+	// 		top = $(id).offset().top;
+	// 	//анимируем переход на расстояние - top за 1500 мс
+
+	// 	$('body,html').animate({
+	// 		scrollTop: top
+	// 	}, 1000);
+	// });
+
+	//плавные якоря для перехода по всем страницам
+	$('a[href*="#"]').on("click", function (e) {
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
 		}, 1000);
+		e.preventDefault();
+		return false;
 	});
+
 
 	//m-menu
 	$('.m-menu').on('click', function (e) {
@@ -29,5 +41,16 @@ $(function () {
 		slidesToScroll: 1
 	});
 
+
+	//модальное окно
+	var elements = $('.modal-overlay, .modal');
+
+	$('.modal-btn').click(function () {
+		elements.addClass('active-modal');
+	});
+
+	$('.close-modal').click(function () {
+		elements.removeClass('active-modal');
+	});
 
 });
