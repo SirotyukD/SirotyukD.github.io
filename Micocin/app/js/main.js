c02404f52274dabb927da325b7fb35d11f920e2f
Sirotuk <sirotuk93@gmail.com>
2019-11-08 09:52:55 +0200

    добавил готовый проэкт Micocin

--- Micocin/app/js/main.js
+++ Micocin/app/js/main.js
@@ -2,24 +2,6 @@ $(function () {
 
 	// Custom JS
 
-	//плавные якоря для перехода по всем страницам
-	$('a[href*="#"]').on("click", function (e) {
-		var anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top
-		}, 1000);
-		e.preventDefault();
-		return false;
-	});
-
-
-	//m-menu
-	$('.m-menu').on('click', function (e) {
-		e.preventDefault();
-		$('.m-menu-box').toggleClass('show');
-	});
-
-
 	//модальное окно
 	var elements = $('.modal-overlay, .modal');
 
@@ -32,21 +14,27 @@ $(function () {
 	});
 
 
-	//анимация кнопки
-	$(".ripple").on("click", function (event) {
-		$(this).append("<span class='ripple-effect'>");
-		$(this).find(".ripple-effect").css({
-			left: event.pageX - $(this).position().left,
-			top: event.pageY - $(this).position().top
-		}).animate({
-			opacity: 0,
-		}, 1500, function () {
-			$(this).remove();
-		});
+	//анимация при скролле
+	AOS.init({
+		// Global settings:
+		disable: 'phone', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+		startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+		initClassName: 'aos-init', // class applied after initialization
+		animatedClassName: 'aos-animate', // class applied on animation
+		useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+		disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+		debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+		throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 120, // offset (in px) from the original trigger point
+		delay: 0, // values from 0 to 3000, with step 50ms
+		duration: 400, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: false, // whether animation should happen only once - while scrolling down
+		mirror: false, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
 	});
-
-
-	//scrollBar
-	$('.scrollbar-macosx').scrollbar();
-
+	
 });
