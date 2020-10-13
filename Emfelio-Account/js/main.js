436359a60e4264a1c5586ef2a2cf47ef3a4e6788
Sirotuk <sirotuk93@gmail.com>
2020-10-13 12:01:12 +0300

    E-A

--- Emfelio-Account/js/main.js
+++ Emfelio-Account/js/main.js
@@ -3,38 +3,8 @@ $(document).ready(function () {
 	var sbw = window.innerWidth - document.documentElement.clientWidth;
 	document.documentElement.style.setProperty('--scrollbar-width', sbw + 'px');
 
-	// var wrapper = document.querySelector(".wrapper");
-	// var burger = document.querySelector(".burger");
-	// var sidebar = document.querySelector(".sidebar-left");
-	// if (burger) {
-	// 	burger.addEventListener("click", function () {
-	// 		wrapper.classList.toggle("active");
-	// 		sidebar.classList.toggle("small");
-	// 	});
-	// }
-
-
-
-	// jQuery('.burger').click(function (event) {
-	// 	// event.preventDefault();
-
-	// 	if (jQuery('.wrapper').hasClass('active')) {
-	// 		jQuery('.wrapper').removeClass('active');
-	// 		jQuery(".sidebar-left").addClass('big');
-	// 		setTimeout(function () {
-	// 			jQuery(".sidebar-left").removeClass('small');
-	// 		}, 300);
-	// 	} else {
-	// 		jQuery(".sidebar-left").removeClass('big');
-	// 		jQuery('.wrapper').addClass('active');
-	// 		setTimeout(function () {
-	// 			jQuery(".sidebar-left").addClass('small');
-	// 		}, 300);
-	// 	}
-	// });
-
+	//функционал выезжания бокового меню
 	jQuery('.burger').click(function (event) {
-		// event.preventDefault();
 
 		if (jQuery(this).hasClass('active')) {
 			jQuery('.burger').removeClass('active');
@@ -43,8 +13,7 @@ $(document).ready(function () {
 			setTimeout(function () {
 				jQuery(".sidebar-left").removeClass('small');
 				jQuery(".sidebar-left").addClass('big');
-			}, 450);
-
+			}, 300);
 
 		} else {
 			jQuery('.wrapper').removeClass('lgMenu');
@@ -54,10 +23,14 @@ $(document).ready(function () {
 				jQuery(".sidebar-left").removeClass('big');
 				jQuery(".sidebar-left").addClass('small');
 			}, 250);
-
-
-
 		}
+
+	});
+
+
+	//добавить класс active для смены цвета input[type="tel"
+	jQuery('body.emfelio-account input[type="tel"]').click(function (event) {
+		jQuery(this).addClass('active')
 	});
 
 
